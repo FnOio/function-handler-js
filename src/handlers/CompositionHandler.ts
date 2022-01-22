3183e5b0335ff2afed651d4b047b65206ce6d37c
Gertjan De Mulder <gertjan.demulder@ugent.be>
2022-01-22 22:02:35 +0100

    Fixed linting errors

--- src/handlers/CompositionHandler.ts
+++ src/handlers/CompositionHandler.ts
@@ -1,33 +1,34 @@
 import { Handler } from './Handler';
 import prefixes from '../prefixes';
 import { ImplementationHandler } from './ImplementationHandler';
-import {Function} from '../models';
+import { Function } from '../models';
 
 type CompositionHandlerOptions = {
-    compositionFn: string,
-    composedOfValueMap: any,
-    dependencyMeta: {
-        [compositionFn: string]: {
-            type: string
-            fn: Function,
-            inputs: {
-                [predicate: string]: string[]
-            },
-            outputs: {
-                [predicate: string]: string[]
-            },
-        }
+  compositionFn: string,
+  composedOfValueMap: any,
+  dependencyMeta: {
+    [compositionFn: string]: {
+      type: string
+      fn: Function,
+      inputs: {
+        [predicate: string]: string[],
+      },
+      outputs: {
+        [predicate: string]: string[],
+      },
     },
-    dependencyList: any,
-    implementationHandler: ImplementationHandler,
-}
+  },
+  dependencyList: any,
+  implementationHandler: ImplementationHandler,
+};
 
 export class CompositionHandler extends Handler {
-    constructor() {
-        super(`${prefixes.fnoi}CompositionFunction`)
-    }
+  constructor() {
+    super(`${prefixes.fnoi}CompositionFunction`);
+  }
 
-    async executeFunction(args: { [key: string]: any[] }, options: CompositionHandlerOptions): Promise<{ [key: string]: any }> {
+  async executeFunction(args: { [key: string]: any[] }, options: CompositionHandlerOptions):
+    Promise<{ [key: string]: any }> {
         // - composedOfMap
         // composedOf1: aValue
         // composedOf2: bValue
@@ -36,7 +37,7 @@ export class CompositionHandler extends Handler {
         // composedOf5: null
         // - functionMaps
         // sum31:
-        //   inputs: 
+        //   inputs:
         //     a: [composedOf1]
         //     b: [composedOf2]
         //   outputs:
@@ -60,37 +61,40 @@ export class CompositionHandler extends Handler {
         //     sum32o: ['sum32'],
         // }
         // - fill in inputs in composedOfMap
-        const compositionFn = options.compositionFn;
-        Object.keys(options.dependencyMeta[compositionFn].inputs).forEach(predicate => {
-            options.dependencyMeta[compositionFn].inputs[predicate].forEach(compositionId => {
-                options.composedOfValueMap[compositionId] = args[predicate];
-            });
-        })
+    const compositionFn = options.compositionFn;
+    Object.keys(options.dependencyMeta[compositionFn].inputs).forEach((predicate) => {
+      options.dependencyMeta[compositionFn].inputs[predicate].forEach((compositionId) => {
+        options.composedOfValueMap[compositionId] = args[predicate];
+      });
+    });
         // - for function of dependencygraph of non-composed functions
         //   execute functionMap and fill in composedOfMap
-        for (const id of options.dependencyList) {
-            if (options.dependencyMeta[id].type !== 'function' || id === compositionFn) {
-                continue;
-            }
-            const implementationId = options.implementationHandler.getImplementations(options.dependencyMeta[id].fn.id)[0];
-            const thisArgs = {};
-            for (const predicate in options.dependencyMeta[id].inputs) {
-                thisArgs[predicate] = options.composedOfValueMap[options.dependencyMeta[id].inputs[predicate][0]];
-            }
-            const results = await options.implementationHandler.executeImplementation(implementationId, thisArgs);
-            Object.keys(results).forEach(predicate => {
-                options.dependencyMeta[id].outputs[predicate].forEach(compositionId => {
-                    options.composedOfValueMap[compositionId] = results[predicate];
-                });
-            })
-        }
-        // - return outputs of composedOfMap
-        const results = {};
-        Object.keys(options.dependencyMeta[compositionFn].outputs).forEach(predicate => {
-            options.dependencyMeta[compositionFn].outputs[predicate].forEach(compositionId => {
-                results[predicate] = options.composedOfValueMap[compositionId];
-            });
+    for (const id of options.dependencyList) {
+      if (options.dependencyMeta[id].type !== 'function' || id === compositionFn) {
+        continue;
+      }
+      const implementationId = options.implementationHandler
+        .getImplementations(options.dependencyMeta[id].fn.id)[0];
+      const thisArgs = {};
+      for (const predicate in options.dependencyMeta[id].inputs) {
+        thisArgs[predicate] = options
+          .composedOfValueMap[options.dependencyMeta[id].inputs[predicate][0]];
+      }
+      const results = await options.implementationHandler
+        .executeImplementation(implementationId, thisArgs);
+      Object.keys(results).forEach((predicate) => {
+        options.dependencyMeta[id].outputs[predicate].forEach((compositionId) => {
+          options.composedOfValueMap[compositionId] = results[predicate];
         });
-        return results;
+      });
     }
+        // - return outputs of composedOfMap
+    const results = {};
+    Object.keys(options.dependencyMeta[compositionFn].outputs).forEach((predicate) => {
+      options.dependencyMeta[compositionFn].outputs[predicate].forEach((compositionId) => {
+        results[predicate] = options.composedOfValueMap[compositionId];
+      });
+    });
+    return results;
+  }
 }
