70470d84486a70db22a93e1c63f5cfc32df49f71
Gertjan De Mulder <gertjan.demulder@ugent.be>
2022-01-25 10:28:57 +0100

    lifted minimalFunctionTests helper

--- src/FunctionHandler.test.ts
+++ src/FunctionHandler.test.ts
@@ -177,25 +177,48 @@ describe('Workflow', () => {
     const fnAB = await handler.getFunction(`${prefixes.fns}functionAB`);
     const functionArray = [fnA, fnB, fnC];
     // Minimal tests that every function must pass
-    const minimalFunctionTests = (f) => {
-      expect(f).not.to.be.null;
-      expect(f.id).not.to.be.null;
-    };
     functionArray.forEach(minimalFunctionTests);
     // Map function labels to JS implementations
     const functionJavaScriptImplementations = {
-      functionA: x => `A(${x})`,
-      functionB: x => `B(${x})`,
-      functionC: x => `C(${x})`,
+      functionA: (x) => `A(${x})`,
+      functionB: (x) => `B(${x})`,
+      functionC: (x) => `C(${x})`,
     };
     // Load JS implementations
     const jsHandler = new JavaScriptHandler();
     Object.entries(functionJavaScriptImplementations).forEach(([lbl, fn]) => {
-      handler.implementationHandler.loadImplementation(`${prefixes.fns}${lbl}Implementation`, jsHandler, { fn },);
+      handler.implementationHandler.loadImplementation(`${prefixes.fns}${lbl}Implementation`, jsHandler, { fn });
     });
 
     const resultAB = await handler.executeFunction(fnAB, { [`${prefixes.fns}str0`]: 1 });
     expect(resultAB[`${prefixes.fns}out`]).to.equal('B(A(1))');
-    return;
+  });
+  it('Test composition AB', async () => {
+    // load composition resources
+    await loadFunctionResource(`${prefixes.fns}compositionAB`, readFile(path.resolve(dirResources, 'workflow/compositionAB.ttl')));
+    // function objects
+
+    const fnA = await handler.getFunction(`${prefixes.fns}functionA`);
+    const fnB = await handler.getFunction(`${prefixes.fns}functionB`);
+    const fnC = await handler.getFunction(`${prefixes.fns}functionC`);
+    const fnAB = await handler.getFunction(`${prefixes.fns}functionAB`);
+    const functionArray = [fnA, fnB, fnC];
+    // Minimal tests that every function must pass
+    functionArray.forEach(minimalFunctionTests);
+    // Map function labels to JS implementations
+    const functionJavaScriptImplementations = {
+      functionA: (x) => `A(${x})`,
+      functionB: (x) => `B(${x})`,
+      functionC: (x) => `C(${x})`,
+    };
+    // Load JS implementations
+    const jsHandler = new JavaScriptHandler();
+    Object.entries(functionJavaScriptImplementations).forEach(([lbl, fn]) => {
+      handler.implementationHandler.loadImplementation(`${prefixes.fns}${lbl}Implementation`, jsHandler, { fn });
+    });
+
+    const resultAB = await handler.executeFunction(fnAB, { [`${prefixes.fns}str0`]: 1 });
+    expect(resultAB[`${prefixes.fns}out`]).to.equal('B(A(1))');
+  });
   });
 });
