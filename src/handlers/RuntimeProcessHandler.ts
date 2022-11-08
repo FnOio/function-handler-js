531f3cc062965a0d657ce3dd07bdeb20ddd2246d
Gertjan De Mulder <gertjan.demulder@ugent.be>
2022-11-08 22:13:40 +0100

    add RuntimeProcessHandler (wip)

---
+++ src/handlers/RuntimeProcessHandler.ts
@@ -0,0 +1,30 @@
+import { Handler } from './Handler';
+import prefixes from '../prefixes';
+
+export class RuntimeProcessHandler extends Handler {
+    constructor() {
+        super(`${prefixes.fnoi}RuntimeProcessHandler`)
+    }
+
+    async executeFunction(args: {[predicate: string]: any}, options: any): Promise<{ [key: string]: any }> {
+        console.log('RuntimeProcessHandler.executeFunction( ... )');
+        const fnArgs: any[] = [];
+        
+        if (Object.keys(options.args.positionArgs).length > 0) {
+            const maxArg = Object.keys(options.args.positionArgs).map(k => Number(k)).sort().reverse()[0];
+            for (let index = 0; index <= maxArg; index++) {
+                if (options.args.positionArgs[index]) {
+                    fnArgs.push(args[options.args.positionArgs[index]])
+                } else {
+                    fnArgs.push(undefined);
+                }
+            }
+        }
+        const fnResult = await options.fn.apply(null, fnArgs);
+        const result = {};
+        if (options.returns['_default']) {
+            result[options.returns['_default']] = fnResult;
+        }
+        return result;
+    }
+}
