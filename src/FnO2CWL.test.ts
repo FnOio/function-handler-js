abdff707764d759aef262e52e97283ca5f515176
Gertjan De Mulder <gertjan.demuder@ugent.be>
2022-11-08 15:22:14 +0100

    (WIP) tests for example01/concrete-wf

--- src/FnO2CWL.test.ts
+++ src/FnO2CWL.test.ts
@@ -14,16 +14,44 @@ function writeFile(path, data) {
   return fs.writeFileSync(path, data,{ encoding: 'utf-8' });
 }
 
-const dirResources = path.resolve(__dirname, '../resources');
-const fnTtl = readFile(path.resolve(dirResources, 'sum.ttl'));
-const fnTtlComposition = readFile(path.resolve(dirResources, 'sum-composition.ttl'));
+const dirResources = path.resolve(__dirname, '../resources/fno-cwl');
+const basenameConcreteWorkflowCWL = 'concrete-wf.cwl'
+const basenameConcreteWorkflowTurtle = `${basenameConcreteWorkflowCWL}.ttl`
 
-describe('FnO 2 CWL tests', () => { // the tests container
+describe('Tests for example01', () => { // the tests container
+  
+  const dirContainerResources = path.resolve(dirResources, 'example01');
+  const pathConcreteWorkflow = path.resolve(dirContainerResources, basenameConcreteWorkflowCWL);
   
   it('tests',async () => {
     console.log("Tada!");
+  });
+
+  it('Contains FnO document of the concrete workflow ',async () => {
+    expect(fs.existsSync(pathConcreteWorkflow));
     
-  })
+  });
+
+  it('Contains FnO document of the abstract workflow ',async () => {
+    throw Error('Not Yet Implemented');
+    
+  });
+
+  it('Correctly loads concrete workflow',async () => {
+    throw Error('Not Yet Implemented');
+    // TODO: loads without errors
+    // TODO: workflow (i.e. the Function Composition) has the expected input parameters
+    // TODO: workflow (i.e. the Function Composition) has the expected output parameters
+    // TODO: workflow (i.e. the Function Composition) has the expected steps/tasks/functions?
+    //  i.e. echo + uppercase
+  });
+
+  it('Correctly executes the concrete workflow',async () => {
+    throw Error('Not Yet Implemented');
+    // TODO: executes without errors?
+    // TODO: produces the expected results?
+  });
+
 
 });
 
