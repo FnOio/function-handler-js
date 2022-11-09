771ec4592bfc4cfbd4962900df7e416f2bf1d937
Gertjan De Mulder <gertjan.demuder@ugent.be>
2022-11-09 10:46:12 +0100

    Tests for paper/example01 (wip)

--- src/FnO2CWL.test.ts
+++ src/FnO2CWL.test.ts
@@ -5,6 +5,7 @@ import { JavaScriptHandler } from './handlers/JavaScriptHandler';
 import prefixes from './prefixes';
 import * as fs from 'fs';
 import * as path from 'path';
+import { RuntimeProcessHandler } from './handlers/RuntimeProcessHandler';
 
 function readFile(path) {
   return fs.readFileSync(path, { encoding: 'utf-8' });
@@ -26,9 +27,21 @@ describe('Tests for example01', () => { // the tests container
   
   const dirContainerResources = path.resolve(dirResources, 'example01');
   const pathConcreteWorkflow = path.resolve(dirContainerResources, basenameConcreteWorkflowTurtle);
-  const base = 'file:///Users/gertjandm/Git/KNoWS/projects/WORKFLOWS/fno-cwl/paper/example01/#';
-  // Currently storing these specific prefixes/namespaces in a local object
 
+  const jsFunctionImplementations = {
+
+    echo: function(message:string) {
+      console.log(`echo(${message})`)
+      return message;
+    },
+
+    uppercase: function(message:string) {
+      return message.toUpperCase();
+    }
+
+  }
+
+  // Currently storing these specific prefixes/namespaces in a local object
   const ns = {
     gdm: "http://gddmulde.be#",
     wf: "http://gddmulde.be/concrete-wf.cwl#",
@@ -36,27 +49,25 @@ describe('Tests for example01', () => { // the tests container
     t_uc: "http://gddmulde.be/uppercase.cwl#",
   }
 
-  it('tests',async () => {
-    console.log("Tada!");
-  });
-
   it('Contains FnO document of the concrete workflow',async () => {
     expect(fs.existsSync(pathConcreteWorkflow));
-    
   });
 
   it('Contains FnO document of the abstract workflow',async () => {
     throw Error('Not Yet Implemented');
-    
   });
 
 
+  /**
+   * Tests loading of functions within the composition (echo, uppercase)
+   * Tests results from executing echo and uppercase
+   */
   it.only('Correctly loads concrete workflow',async () => {
     const handler = new FunctionHandler();
+    const rtpHandler = new RuntimeProcessHandler();
+    const jsHandler = new JavaScriptHandler();
     
-    // Load FnO workflow description
-    console.log('!!!', ns.gdm)
-    
+    // Load FnO descriptions
     const iriConcreteWorkflow = prefix(ns.gdm, 'workflowGraph');
     await handler.addFunctionResource(
       iriConcreteWorkflow,
@@ -77,6 +88,7 @@ describe('Tests for example01', () => { // the tests container
     const fEcho = await handler.getFunction(iriEcho);
     const fUppercase = await handler.getFunction(iriUppercase);
 
+    // Test loaded function objects
     expect(fWf).not.to.be.null;
     expect(fWf.id).not.to.be.null;
 
@@ -85,20 +97,43 @@ describe('Tests for example01', () => { // the tests container
 
     expect(fUppercase).not.to.be.null;
     expect(fUppercase.id).not.be.null;
-    
-    // const fWf = await handler.getFunction(prefix());
-    // expect(fWf).not.to.be.null;
 
-    // TODO: workflow (i.e. the Function Composition) has the expected input parameters
-    // TODO: workflow (i.e. the Function Composition) has the expected output parameters
-    // TODO: workflow (i.e. the Function Composition) has the expected steps/tasks/functions?
-    //  i.e. echo + uppercase
+    // Instantiate implementation handlers
+    const iriEchoImplementation = prefix(ns.t_echo, 'Implementation');
+    const iriUppercaseImplementation = prefix(ns.t_uc, 'Implementation');
+    
+    // Implementation by RuntimeProcessHandler
+    handler.implementationHandler.loadImplementation(
+      iriEchoImplementation,
+      rtpHandler,
+      { baseCommand: "echo" } // TODO: function object??
+    );
+
+    // Implementation by JavascriptHandler
+    handler.implementationHandler.loadImplementation(
+      iriUppercaseImplementation,
+      jsHandler,
+      { fn: jsFunctionImplementations.uppercase }
+    );
+
+    // Test echo output
+    const fnEchoArgMap = {
+      [prefix(ns.t_echo, 'message')]: 'abc'
+    }
+    const fnEchoResult = await handler.executeFunction(fEcho, fnEchoArgMap);
+    expect(fnEchoResult[prefix(ns.t_echo, 'out')]).to.equal('abc\n');
+
+    // Test uppercase output
+    const fnUppercaseArgMap = {
+      [prefix(ns.t_uc, 'message')]: 'abc'
+    }
+    const fnUppercaseResult = await handler.executeFunction(fUppercase, fnUppercaseArgMap)
+    expect(fnUppercaseResult[prefix(ns.t_uc, 'uppercase_message')]).to.equal('ABC');
+    
   });
 
   it('Correctly executes the concrete workflow',async () => {
     throw Error('Not Yet Implemented');
-    // TODO: executes without errors?
-    // TODO: produces the expected results?
   });
 
 
