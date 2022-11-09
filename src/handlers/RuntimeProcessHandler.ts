c3560a465c8d1d13b5b8bcc6e01b4db9911a1670
Gertjan De Mulder <gertjan.demuder@ugent.be>
2022-11-09 10:43:45 +0100

    Execute basecommand through child_process

--- src/handlers/RuntimeProcessHandler.ts
+++ src/handlers/RuntimeProcessHandler.ts
@@ -1,13 +1,12 @@
 import { Handler } from './Handler';
 import prefixes from '../prefixes';
-
+import {exec} from "child_process";
 export class RuntimeProcessHandler extends Handler {
     constructor() {
-        super(`${prefixes.fnoi}RuntimeProcessHandler`)
+        super(`${prefixes.fnoi}RuntimeProcessHandler`);
     }
 
     async executeFunction(args: {[predicate: string]: any}, options: any): Promise<{ [key: string]: any }> {
-        console.log('RuntimeProcessHandler.executeFunction( ... )');
         const fnArgs: any[] = [];
         
         if (Object.keys(options.args.positionArgs).length > 0) {
@@ -20,7 +19,24 @@ export class RuntimeProcessHandler extends Handler {
                 }
             }
         }
-        const fnResult = await options.fn.apply(null, fnArgs);
+        
+        // Build command to execute
+        const cmd = [options.baseCommand, ...fnArgs].join(' ');
+        const execPromise = new Promise((resolve,reject)=>{
+            exec(cmd, (error,stdout,stderr)=>{
+                if (error) {
+                    reject(error)
+                    return;
+                }
+                if (stderr) {
+                    reject(stderr);
+                    return;
+                }
+                resolve(stdout)
+            })
+        })
+        
+        const fnResult = await execPromise;
         const result = {};
         if (options.returns['_default']) {
             result[options.returns['_default']] = fnResult;
